1323a0ef7c80c2c8fac210ec4b894f2d1d3e0596
kazu1222 <12.22.96.kazu@gmail.com>
2024-03-24 01:10:08 +0900

    検索結果情報取得処理作成

--- client/src/pages/search.jsx
+++ client/src/pages/search.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import axios from 'axios';
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
 
 const search = () => {
+    const router = useRouter();
+    const {query: searchQuery} = router.query;
+    console.log(searchQuery)
+
+    useEffect(() => {
+        if(!searchQuery) {
+            return
+        }
+        const fetchMedia = async() => {
+            try {
+                const response = await axios.get(`api/searchMedia?searchQuery=${searchQuery}`)
+                console.log(response);
+            } catch(err){
+                console.log(err);
+            }
+        }
+
+        fetchMedia()
+
+    },[searchQuery])
   return (
-    <div>search</div>
-  )
+        <div>Seach</div>
+    )
 }
 
 export default search
