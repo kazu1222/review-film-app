599accc0342cec3aef0585e1fb5337fa474977ca
kazu1222 <12.22.96.kazu@gmail.com>
2024-03-23 18:06:34 +0900

    Home画面更新

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,7 +1,27 @@
-import AppLayout from '@/components/Layouts/AppLayout'
-import Head from 'next/head'
+import AppLayout from '@/components/Layouts/AppLayout';
+import axios from 'axios';
+import Head from 'next/head';
+import { useEffect, useState } from 'react';
 
-const Dashboard = () => {
+const Home = () => {
+    const[movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        const fetchMovies = async() => {
+            try{
+                const response = await axios.get('api/getPopularMovies');
+                // console.log(response.data.results);
+                setMovies(response.data.results);
+                console.log(movies);
+            } catch(err){
+                console.log(err);
+            }
+        }
+        
+        fetchMovies();
+
+    }, [])
+
     return (
         <AppLayout
             header={
@@ -13,17 +33,11 @@ const Dashboard = () => {
                 <title>Laravel - Dashboard</title>
             </Head>
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 bg-white border-b border-gray-200">
-                            You're logged in!
-                        </div>
-                    </div>
-                </div>
-            </div>
+            {movies.map((movie) => (
+                <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}/>
+            ))}
         </AppLayout>
     )
 }
 
-export default Dashboard
+export default Home
