d4e1cfe36f9cba89f0daa822636f65babf7eb0cd
kazu1222 <12.22.96.kazu@gmail.com>
2024-03-23 18:39:13 +0900

    MUIスタイル追加

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -2,6 +2,9 @@ import AppLayout from '@/components/Layouts/AppLayout';
 import axios from 'axios';
 import Head from 'next/head';
 import { useEffect, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import { CardActionArea, CardMedia, Typography } from '@mui/material';
 
 const Home = () => {
     const[movies, setMovies] = useState([]);
@@ -32,10 +35,53 @@ const Home = () => {
             <Head>
                 <title>Laravel - Dashboard</title>
             </Head>
+            <Swiper
+                spaceBetween={30}
+                slidesPerView={5}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+                breakpoints={{
+                     // 320px以上の画面サイズの場合
+                     320: {
+                        slidesPerView: 1,
+                        spaceBetween: 10,
+                    },
+                    // 480px以上の画面サイズの場合
+                    480: {
+                        slidesPerView: 3,
+                        spaceBetween: 20,
+                    },
+                    // 640px以上の画面サイズの場合
+                    640: {
+                        slidesPerView: 4,
+                        spaceBetween: 30,
+                    },
+                    // 768px以上の画面サイズの場合
+                    768: {
+                        slidesPerView: 5,
+                        spaceBetween: 40,
+                    },
+                }}
+                >
+                {movies.map((movie) => (
+                    <SwiperSlide key={movie.id}>
+                        <CardActionArea>
+                            <CardMedia
+                                compornent={"img"}
+                                sx={{
+                                        aspectRatio: '2/3',
+                                    }}
+                                    image={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                                    alt={movie.title}
+                            />
+                        </CardActionArea>
 
-            {movies.map((movie) => (
-                <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}/>
-            ))}
+                        <Typography>
+                            公開日：{movie.release_date}
+                        </Typography>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
         </AppLayout>
     )
 }
