95ac2af2ae35a87a75bc1119c26eebb5a3d36b8d
kazu1222 <12.22.96.kazu@gmail.com>
2024-04-03 14:52:57 +0900

    お気に入り一覧ページ作成

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -49,4 +49,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Review::class);
     }
+
+    public function favorites()
+    {
+        return $this->hasMany(Favorite::class);
+    }
 }
